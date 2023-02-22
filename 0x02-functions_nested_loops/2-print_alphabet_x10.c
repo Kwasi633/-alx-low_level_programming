63eeb4365359309df855ff1d7ab1fb6581833823
Ezra Asiedu <ezrakwasi633@gmail.com>
2023-02-22 07:41:59 -0800

    alpa x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets
+ * 10 times and a new line.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char al;
+	int x;
+
+	for(x = 1; x <= 10; x++)
+	{
+		for (al = 'a'; al <= 'z'; al++)
+		{
+			_putchar(al);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,5 @@
 int main(void);
 int _putchar(int);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif
