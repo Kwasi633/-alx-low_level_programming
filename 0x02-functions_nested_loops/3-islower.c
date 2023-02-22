49985e6f29c071ed68f98b064da04ead71812859
Ezra Asiedu <ezrakwasi633@gmail.com>
2023-02-22 08:20:58 -0800

    low

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower() - prints lowercase char
+ *
+ * Return: 0 Always.
+ */
+int _islower(int c)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+
+	return (0);
+}
