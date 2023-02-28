385a8fbf7cf776c047dd61ddf8ba17c4794bbfa7
Ezra Asiedu <ezrakwasi633@gmail.com>
2023-02-28 01:48:46 -0800

    *n = 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - writes a function that uses an int 
+ * pointer paramater and chamges the value it points to 90.
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
