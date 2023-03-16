b7bd0580dd4d338cca1cecfad85f09e1ccb3be08
Ezra Asiedu <ezrakwasi633@gmail.com>
2023-03-15 19:04:58 -0700

    Task 1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,32 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory
+ * str_concat - returns a pointer to a newly allocated space in memory
  * which contains a copy of the str given as a parameter.
- * @str: The str
+ * @s1: The str to be joined
+ * @s2: The str to join s2
  *
  * Return: returns a pointer to the duplicated str
  * It returns NULL if insufficient memory was available
  */
-char *_strdup(char *str)
+
+char *_str_concat(char *s1, char *s2)
 {
-	char *copy;
-	int i, len = 0;
+	char *join;
+	int copy = 0;
+	int length = 0;
+	int i;
 
-	if (str == NULL)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] || s2[i]; i++)
+		length++;
+
+	join = malloc(sizeof(char) * length);
+
+	if (join == NULL)
 		return (NULL);
 
-	while (str[len] != '\0')
-		len++;
+	for (i = 0; s1[i] < len; i++)
+		join[copy++] = s1[i];
 
-	copy = (char *)malloc((sizeof(char) * len) + 1);
-	if (copy == NULL)
-		return (NULL);
+	for (i = 0; s2[i]; i++)
+		join[copy++] = s2[i];
 
-	for (i = 0; i < len; i++)
-		copy[i] = str[i];
-	copy[len] = '\0';
-
-	return (copy);
+	return (join);
 }
