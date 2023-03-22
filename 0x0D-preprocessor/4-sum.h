cea1d24856de286f6aa4a35263af1510e1a50b7b
Ezra Asiedu <ezrakwasi633@gmail.com>
2023-03-21 23:06:33 -0700

    complete tasks

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Descp: Header that defines a fun macro SUM(x, y)
+ * that computes the sum of the numbers x and y.
+ * Headerfile guarded to avoid double inclusion.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
